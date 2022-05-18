4c1e5532f6a460bc825e1cda2d6193c2ba1e8210
Valentin <valio31032@gmail.com>
2022-05-18 12:55:24 +0100

    exercise 4.10

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,20 +1,35 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
 const api = supertest(app)
 
 test('registered blogs', async () => {
-	const response = await api.get('/api/blogs')
+	const response = await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
 	expect(response.body).toHaveLength(2)
 }, 100000)
 
 test('property "id" exists', async () => {
-	const blogs = await Blog.find({})
+	const blogs = await helper.blogsInDb()
 	const firstBlog = blogs[0]
 	expect(firstBlog.id).toBeDefined()
 }, 100000)
+
+test('blog added successfully', async () => {
+	const blogsBeforeUpdate = await helper.blogsInDb()
+	const newBlog = {
+		title: 'test1',
+		author: 'test mctesty',
+		url: 'test.test',
+		likes: 2093
+	}
+	await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type', /application\/json/)
+
+	const updatedBlogs = await helper.blogsInDb()
+	expect(updatedBlogs).toHaveLength(blogsBeforeUpdate.length + 1)
+
+})
 
 afterAll(() => {
 	mongoose.connection.close()
