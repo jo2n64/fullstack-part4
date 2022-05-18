54cc49d943c7a63d104788dd0a06420e75fd40d6
Valentin <valio31032@gmail.com>
2022-05-18 12:43:56 +0100

    exercise 4.9

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
 test('registered blogs', async () => {
 	const response = await api.get('/api/blogs')
 	expect(response.body).toHaveLength(2)
+}, 100000)
+
+test('property "id" exists', async () => {
+	const blogs = await Blog.find({})
+	const firstBlog = blogs[0]
+	expect(firstBlog.id).toBeDefined()
 }, 100000)
 
 afterAll(() => {
