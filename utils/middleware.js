17e742e04c3fa466310603cb5c7c01ed359b00db
Valentin <valio31032@gmail.com>
2022-05-19 13:26:54 +0100

    exercise 4.18 and 4.19

--- utils/middleware.js
+++ utils/middleware.js
@@ -20,12 +20,16 @@ const errorHandler = (error, request, response, next) => {
 	} else if (error.name === 'ValidationError') {
 		return response.status(400).json({ error: error.message })
 	}
+	else if (error.name === 'JsonWebTokenError') {
+		return response.status(401).json({ error: 'invalid token' })
+	}
+
+	logger.error(error.message)
 
 	next(error)
-}
 
-module.exports = {
-	requestLogger,
-	unknownEndpoint,
-	errorHandler
-}
+	module.exports = {
+		requestLogger,
+		unknownEndpoint,
+		errorHandler
+	}
